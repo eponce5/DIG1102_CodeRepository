2720c44ec8bf236b0e35aa0761493d34ce9f17b4
eponce5 <eponce5@mail.valenciacollege.edu>
2021-02-07 20:52:23 -0500

    script.js

---
+++ Conditionals/script.js
@@ -0,0 +1,30 @@
+
+
+    var userChoice = prompt("heads or tails?");
+
+    var computerChoice = Math.random();
+    Math.floor(Math.random() * 2);
+    if (Math.random() < 0.5) {
+        println("heads!");
+      } else {
+        println("tails!");
+      }
+    console.log(y)
+    console.log(computerChoice);
+    var heads = computerChoice <= 0;
+    var tails = computerChoice >= 1;
+
+    var val = randomInt(0, 1);
+if (val === 0 ) {
+  println("Heads");
+  var val = randomInt(0, 1);
+if (val === 2 ) {
+  println("Tails");
+   
+}
+var userChoice = prompt("What year were you born?");
+    const birthYear = 2001; n 
+} if (( 2021 - birthYear) < 21 ) {
+ alert ("Sorry, you are not old enough to drink in the US");
+
+}
